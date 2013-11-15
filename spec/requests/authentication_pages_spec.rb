67e6dc08ac48f2ea2e6a8e81312917a359a72253
Jose <jlfernandez83@gmail.com>
2013-11-15 10:13:04 +0100

    Problemas con tests arreglados

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -4,8 +4,14 @@ describe "Authentication" do
 
   subject { page }
 
-  describe "signin" do
+  describe "signin page" do
+    before { visit signin_path }
 
+    it { should have_content('Sign in') }
+    it { should have_title('Sign in') }
+  end
+
+  describe "signin" do
     before { visit signin_path }
 
     describe "with invalid information" do
@@ -20,7 +26,7 @@ describe "Authentication" do
       end
     end
 
-    describe "with valid information" do
+   describe "with valid information" do
       let(:user) { FactoryGirl.create(:user) }
       before do
         fill_in "Email",    with: user.email.upcase
@@ -32,6 +38,10 @@ describe "Authentication" do
       it { should have_link('Profile',     href: user_path(user)) }
       it { should have_link('Sign out',    href: signout_path) }
       it { should_not have_link('Sign in', href: signin_path) }
+      describe "followed by signout" do
+        before { click_link "Sign out" }
+        it { should have_link('Sign in') }
+      end
     end
   end
 end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -1,42 +1,61 @@
 require 'spec_helper'
 
-describe "Authentication" do
+describe "User pages" do
 
   subject { page }
 
-  describe "signin page" do
-    before { visit signin_path }
+  describe "profile page" do
+    let(:user) { FactoryGirl.create(:user) }
+    before { visit user_path(user) }
 
-    it { should have_content('Sign in') }
-    it { should have_title('Sign in') }
+    it { should have_content(user.name) }
+    it { should have_title(user.name) }
   end
 
-  describe "signin" do
-    before { visit signin_path }
+  describe "signup page" do
+    before { visit signup_path }
+
+    it { should have_content('Sign up') }
+    it { should have_title(full_title('Sign up')) }
+  end
+ 
+  describe "signup" do
+
+    before { visit signup_path }
+
+    let(:submit) { "Create my account" }
 
     describe "with invalid information" do
-      before { click_button "Sign in" }
+      it "should not create a user" do
+        expect { click_button submit }.not_to change(User, :count)
+      end
+  
+      describe "after submission" do
+        before { click_button submit }
 
-      it { should have_title('Sign in') }
-      it { should have_selector('div.alert.alert-error', text: 'Invalid') }
+        it { should have_title('Sign up') }
+        it { should have_content('error') }
+      end
     end
 
-  describe "with valid information" do
-      let(:user) { FactoryGirl.create(:user) }
+    describe "with valid information" do
       before do
-        fill_in "Email",    with: user.email.upcase
-        fill_in "Password", with: user.password
-        click_button "Sign in"
+        fill_in "Name",         with: "Example User"
+        fill_in "Email",        with: "user@example.com"
+        fill_in "Password",     with: "foobar"
+        fill_in "Confirmation", with: "foobar"
       end
 
-      it { should have_title(user.name) }
-      it { should have_link('Profile',     href: user_path(user)) }
-      it { should have_link('Sign out',    href: signout_path) }
-      it { should_not have_link('Sign in', href: signin_path) }
+      it "should create a user" do
+        expect { click_button submit }.to change(User, :count).by(1)
+      end
 
-      describe "followed by signout" do
-        before { click_link "Sign out" }
-        it { should have_link('Sign in') }
+      describe "after saving the user" do
+        before { click_button submit }
+        let(:user) { User.find_by(email: 'user@example.com') }
+
+        it { should have_title(user.name) }
+        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
       end
     end
   end
