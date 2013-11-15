55a77d2c4220e12335904bb4457805adda43abf7
Jose <jlfernandez83@gmail.com>
2013-11-15 09:31:09 +0100

    End of chapter 8

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -4,14 +4,8 @@ describe "Authentication" do
 
   subject { page }
 
-  describe "signin page" do
-    before { visit signin_path }
-
-    it { should have_content('Sign in') }
-    it { should have_title('Sign in') }
-  end
-
   describe "signin" do
+
     before { visit signin_path }
 
     describe "with invalid information" do
@@ -19,9 +13,14 @@ describe "Authentication" do
 
       it { should have_title('Sign in') }
       it { should have_selector('div.alert.alert-error', text: 'Invalid') }
+
+      describe "after visiting another page" do
+        before { click_link "Home" }
+        it { should_not have_selector('div.alert.alert-error') }
+      end
     end
 
-  describe "with valid information" do
+    describe "with valid information" do
       let(:user) { FactoryGirl.create(:user) }
       before do
         fill_in "Email",    with: user.email.upcase
@@ -33,11 +32,6 @@ describe "Authentication" do
       it { should have_link('Profile',     href: user_path(user)) }
       it { should have_link('Sign out',    href: signout_path) }
       it { should_not have_link('Sign in', href: signin_path) }
-
-      describe "followed by signout" do
-        before { click_link "Sign out" }
-        it { should have_link('Sign in') }
-      end
     end
   end
 end
