b0967d7c7da173e698fe88ceac4382b6d00cfacb
Jose <jlfernandez83@gmail.com>
2013-11-20 12:48:38 +0100

    Final 9.3.1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,19 @@
 class UsersController < ApplicationController
+  before_action :signed_in_user, only: [:index, :edit, :update] #Este comando simplemente indica que el método signed_in_user se ejecuta antes que las acciones, en este caso, antes que edit y update
+  before_action :correct_user,   only: [:edit, :update]
+  
+  def index
+    @users = User.all
+  end
+
   def show
     @user = User.find(params[:id])
   end
+
   def new
     @user = User.new
   end
+
   def create
     @user = User.new(user_params)
     if @user.save
@@ -16,11 +25,35 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
   private
 
     def user_params
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
     end
+    # Before filters
 
+    def signed_in_user
+      unless signed_in?
+        store_location
+        redirect_to signin_url, notice: "Please sign in."
+      end
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
+    end
 end
